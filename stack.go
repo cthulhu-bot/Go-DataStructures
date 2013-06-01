b02176161346c907252d6f7fe3e7f6806dfa9ab9
Joshua Stupplebeen <jpstup@gmail.com>
2013-05-31 23:01:31 -0600

    started on stack data structure

--- stack.go
+++ stack.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-function main() {
-    fmt.Println("Hello world");
+type Stack struct {
+    stack []int
+    sp *int
+}
+
+func (s Stack) push() {
+}
+
+func (s Stack) pop() int {
+    return 0;
+}
+
+func (s Stack) peek() int {
+    return 0;
+}
+
+func main() {
+    s := Stack{}
+    fmt.Println("Stack init: ", s)
 }
