07266c039eef430dfd4d77e8cb89e7e432ac9d67
Joshua Stupplebeen <jpstup@gmail.com>
2013-06-06 00:26:23 -0600

    must pass pointers to structs and dereference them before calling their attributes

--- stack.go
+++ stack.go
@@ -10,26 +10,27 @@ func newStack(foo []int, bar *int) *Stack {
     return &Stack{foo}
 }
 
-func (s Stack) push(i int) {
-    s.St = append(s.St, i)
+func (s *Stack) push(i int) {
+    (*s).St = append(s.St, i)
 }
 
-func (s Stack) pop() int {
+func (s *Stack) pop() int {
     return 0;
 }
 
-func (s Stack) peek() int {
-    return s.St[len(s.St)]
+func (s *Stack) peek() int {
+    return (*s).St[len(s.St)]
 }
 
-func (s Stack) length() int {
-    return len(s.St)
+func (s *Stack) length() int {
+    return len((*s).St)
 }
 
 func main() {
     s := Stack{[]int{1,2}}
     fmt.Println("Stack init: ", s)
     fmt.Println("Stack length: ", s.length())
-    s.push(2)
-    fmt.Println("Stack push: ", s.St)
+    i := 3
+    s.push(i)
+    fmt.Println("Stack push %d: %s", i, s.St)
 }
