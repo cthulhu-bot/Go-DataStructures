b65563b705d25548a02032ba5cac1bb03b87dc95
Joshua Stupplebeen <jpstup@gmail.com>
2013-06-07 23:14:47 -0600

    unit testing working

--- queue.go
+++ queue.go
@@ -1,8 +1,9 @@
-package main
 
-import "fmt"
+// +build main2
 
-type struct Queue {
+package queue
+
+type Queue struct {
     que []int
 }
 
--- stack_test.go
+++ stack_test.go
@@ -1,9 +1,23 @@
-package main
+package stack
 
-import "testing"
+import (
+        "testing"
+       )
 
-func Test_push (t *Testing.T) {
-}
-
-func Test_pop (t *Testing.T) {
+func Test_push_1 (t *testing.T) {
+    s := Stack{[]int{}}
+    s.push(1)
+    s.push(2)
+    ints := []int{1,2}
+    int_test := true
+    for i := range s.St {
+        if s.St[i] != ints[i] {
+            int_test = false
+        }
+    }
+    if int_test {
+        t.Log("push:  --test1 passed")
+    } else {
+        t.Error("Error in Stack.Push")
+    }
 }
