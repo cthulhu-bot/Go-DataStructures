d057cd3f79536e5267e56404bf11e91b6be2a20d
Joshua Stupplebeen <jpstup@gmail.com>
2013-06-30 13:21:33 -0600

    Working on double linked list

--- src/data-structures/double-linked-list.go
+++ src/data-structures/double-linked-list.go
@@ -15,16 +15,25 @@ type DoubleLinkedList struct {
     tail *Node
 }
 
-func (d *DoubleLinkedList) add(n Node) {
+func (d *DoubleLinkedList) add(n Node, str destination) (error) {
+    err := fmt.Errorf("**ERROR** Invalid destination designated")
+    return err
 }
 
 func (d *DoubleLinkedList) removeData(dat int) (error) {
+    if !d.dataInList(dat) {
+        err := fmt.Errorf("**ERROR** Data: %d not found in list",dat)
+        return err
+    }
 }
 
 func (d *DoubleLinkedList) dataInList(dat int) bool {
 }
 
 func (d *DoubleLinkedList) containsCycle() bool {
+}
+
+func (d *DoubleLinkedList) print() {
 }
 
 func main() {
