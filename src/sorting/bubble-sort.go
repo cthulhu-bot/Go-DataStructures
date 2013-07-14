e327c94d648c21bca268e5b109dfdb589db75c71
Joshua Stupplebeen <jpstup@gmail.com>
2013-07-14 00:53:52 -0600

    working on bubble-sort

--- src/sorting/bubble-sort.go
+++ src/sorting/bubble-sort.go
@@ -2,11 +2,23 @@ package main
 
 import (
         "fmt"
-        "errors"
 )
 
-func bubbleSort(input []int) {
+func bubbleSort(input []int) ([]int) {
+    ptr := input[0]
+    for i:=1; i < len(input); i++ {
+        if i == len(input) {
+            fmt.Println("Reached the end")
+            return input
+        }
+        ptr++
+    }
+    return input
 }
 
 func main() {
+    dat := []int{5,12,18,2,7,9,1,15,8,11}
+
+    ret := bubbleSort(dat)
+    fmt.Println("Final Sort: ", ret)
 }
