96c4f10f55a2927e2fae8ce73e508705706a1b99
Joshua Stupplebeen <jpstup@gmail.com>
2013-07-21 11:17:50 -0600

    skeletonized bubble-sort tests, TDD looks to be really easy in go

---
+++ src/sorting/bubble-sort_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "testing
+)
+
+func Test_sort_already_sorted (t *testing.T) {
+}
+
+func Test_sort_reverse_order (t *testing.T) {
+}
+
+func Test_sort_negative_numbers (t *testing.T) {
+}
+
+func Test_sort_alpha_chars (t *testing.T) {
+}
+
+func Test_sort_invalid_chars (t *testing.T) {
+}
+
+func Test_sort_empty_list (t *testing.T) {
+}
+
+func Test_sort_single_element (t *testing.T) {
+}
