52517ba570e78d8a65e045865989bbb704033d2b
Joshua Stupplebeen <jpstup@gmail.com>
2013-07-28 00:26:12 -0600

    Starting working on binaryTree

---
+++ src/data-structures/binary-tree.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+        "fmt"
+)
+
+type Node struct {
+    data int
+    leftChild *Node
+    rightChild *Node
+}
+
+type BinaryTree struct {
+    head *Node
+}
+
+func (b *BinaryTree) add(n Node) {
+    fmt.Println("Eat a dick")
+}
+
+func (b *BinaryTree) isBalanced() (bool) {
+    return false
+}
+
+func main() {
+//    tree := new (BinaryTree)
+    fmt.Println("Eat a dick")
+}
